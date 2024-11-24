e652874d8738c05ea2024c10cbcb6d8e8bf2ae8e
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-11-24 16:16:06 +0500

    Part One

---
+++ WebApi/Brokers/Storages/IStorageBroker.Student.cs
@@ -0,0 +1,13 @@
+﻿using WebApi.Models.Foundations.Students;
+
+namespace WebApi.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Student> InsertStudentAsync(Student student);
+        ValueTask<IQueryable<Student>> SelectAllStudentsAsync();
+        ValueTask<Student> SelectStudentByIdAsync(Guid studentId);
+        ValueTask<Student> UpdateStudentAsync(Student student);
+        ValueTask<Student> DeleteStudentAsync(Student student);
+    }
+}
---
+++ WebApi/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Brokers.Storages
+{
+    public partial class StorageBroker:DbContext,IStorageBroker
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = this.configuration
+                .GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseMySql(connectionString,
+                new MySqlServerVersion(new Version(8, 0, 39)));
+        }
+        private async ValueTask<T> InsertAsync<T>(T @object)
+        {
+            Entry(@object).State = EntityState.Added;
+            await SaveChangesAsync();
+            DetachEntity(@object);
+
+            return @object;
+        }
+
+        private async ValueTask<IQueryable<T>> SelectAllAsync<T>() where T : class =>
+            Set<T>();
+
+        private async ValueTask<T> SelectAsync<T>(params object[] @objectIds) where T : class =>
+            await FindAsync<T>(@objectIds);
+
+        private async ValueTask<T> UpdateAsync<T>(T @object)
+        {
+            Entry(@object).State = EntityState.Modified;
+            await SaveChangesAsync();
+            DetachEntity(@object);
+
+            return @object;
+        }
+
+        private async ValueTask<T> DeleteAsync<T>(T @object)
+        {
+            Entry(@object).State = EntityState.Deleted;
+            await SaveChangesAsync();
+            DetachEntity(@object);
+
+            return @object;
+        }
+
+        private void DetachEntity<T>(T @object)
+        {
+            Entry(@object).State = EntityState.Detached;
+        }
+    }
+}
